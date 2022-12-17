878463787f74d46a209bd490c5af41c60fab7322
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-17 21:55:14 +0900

    spring study MemberRepositoryTest

--- src/main/java/com/example/springwork/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/springwork/repository/MemoryMemberRepository.java
@@ -32,4 +32,8 @@ public class MemoryMemberRepository implements MemberRepsoitory{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
