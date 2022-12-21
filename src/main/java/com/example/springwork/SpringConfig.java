aa7b84ddc95ca365bd639804568bcd9079f7668a
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-21 19:01:41 +0900

    DB 연결

--- src/main/java/com/example/springwork/SpringConfig.java
+++ src/main/java/com/example/springwork/SpringConfig.java
@@ -1,21 +1,30 @@
 package com.example.springwork;
 
+import com.example.springwork.repository.JdbcMemberRepository;
 import com.example.springwork.repository.MemberRepsoitory;
 import com.example.springwork.repository.MemoryMemberRepository;
 import com.example.springwork.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    
+    private final DataSource dataSource;
 
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepsoitory());
+        return new MemberService(memberRepository());
     }
-
     @Bean
-    public MemberRepsoitory memberRepsoitory() {
-        return new MemoryMemberRepository();
+    public MemberRepsoitory memberRepository() {
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
